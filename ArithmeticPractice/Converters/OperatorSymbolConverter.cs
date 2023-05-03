0d4cc67002e7afcd00636ed56bc8b8a0b9af3dfb
relegated <relegated@outlook.com>
2023-05-03 09:30:35 -0400

    Add data template for the arithmetic question, add converter for the symbol to display in the view

---
+++ ArithmeticPractice/Converters/OperatorSymbolConverter.cs
@@ -0,0 +1,42 @@
+﻿using ArithmeticPractice.Enums;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ArithmeticPractice.Converters
+{
+    public class OperatorSymbolConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var operatorType = (EArithmeticOperator)value;
+            switch (operatorType)
+            {
+                case EArithmeticOperator.Add:
+                    return "+";
+                case EArithmeticOperator.Subtract:
+                    return "-";
+                case EArithmeticOperator.Multiply:
+                    return "×";
+                case EArithmeticOperator.Divide:
+                    return "÷";
+                case EArithmeticOperator.Exponent:
+                case EArithmeticOperator.Logarthim:
+                case EArithmeticOperator.Undefined:
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
